e229f9861cf4de072d45b560baa41f2fc48669cc
JaySurplusYoon <jaytaildish@gmail.com>
2018-11-04 01:14:33 +0900

    haha
    ddd

--- webdriver/click_then_screenshot.py
+++ webdriver/click_then_screenshot.py
@@ -11,19 +11,38 @@ This is a temporary script file.
 4.해당 좌표 클릭
 """
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 import pyautogui
 
-driver = driver = webdriver.Chrome(executable_path=r'C:/Users/jayta/Documents/repo/package/chromedriver.exe')
-driver.implicitly_wait(3)
-url='https://nid.naver.com/nidlogin.login'
+#def find_id():
+#    num=0
+#    id = ['userid', 'id', 'name','username']
+#    for i in id:
+#       driver.find_element_by_xpath('//*[@id='+id[num]+']').send_keys('ID')
+#        num=num+1        
+
+#def find_pass():
+#    num=0
+#    passwd = ['pass','password','pswd','passwd','passwrd']
+#    for i in passwd:
+#        driver.find_element_by_xpath('//*[@id="passwd"]').send_keys('비밀번호')
+#        num=num+1
+        
+
+driver = webdriver.Chrome(executable_path=r'C:/Users/jayta/Documents/repo/package/chromedriver.exe')
+driver.set_page_load_timeout(30)
+
+driver.implicitly_wait(1)
+url='http://htht-3.com/main.asp?abc'
 driver.get(url)
 driver.maximize_window()
-
-driver.find_element_by_name('id').send_keys('jay_tester')
-driver.find_element_by_name('pw').send_keys('dbswodnd')
+driver.find_element_by_xpath('//*[@id="userid"]').send_keys('ID')
+driver.find_element_by_xpath('//*[@id="passwd"]').send_keys('비밀번호'+u'\ue007')
 
 driver.get_screenshot_as_file('C:/Users/jayta/Documents/repo/screenshot/1.png')
+
 pyautogui.size()
 (1366, 768)
-
 pyautogui.click(x=719, y=465, clicks=1, interval=1, button='left')
+
+
