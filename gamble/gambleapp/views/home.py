25fe260e9c14784cbe79277504524fd4d1bf2e73
deadlylaid <deadlylaid@gmail.com>
2018-11-04 04:06:43 +0900

    pep8

--- gamble/gambleapp/views/home.py
+++ gamble/gambleapp/views/home.py
@@ -14,14 +14,13 @@ class HomeView(CreateView):
 
     def form_valid(self, form):
         result = super().form_valid(form)
-        images = ['/Users/hanminsoo/Desktop/github/judge_gamble/gamble/gamble/media/0.jpg']
 
-        jungle_zip = zipfile.ZipFile('/Users/hanminsoo/Desktop/github/judge_gamble/gamble/gamble/media/image.zip','w')
-        for folder, subfolders, files in os.walk('/Users/hanminsoo/Desktop/github/judge_gamble/gamble/gamble/media/image'):
+        jungle_zip = zipfile.ZipFile('/Users/hanminsoo/Desktop/github/judge_gamble/gamble/gamble/media/image.zip', 'w')
+        for folder, subfolders, files in os.walk(
+                '/Users/hanminsoo/Desktop/github/judge_gamble/gamble/gamble/media/image'):
             for file in files:
-                jungle_zip.write(os.path.join(folder, file),compress_type=zipfile.ZIP_DEFLATED)
+                jungle_zip.write(os.path.join(folder, file), compress_type=zipfile.ZIP_DEFLATED)
             pass
-        # jungle_zip.write('/Users/hanminsoo/Desktop/github/judge_gamble/gamble/gamble/media/0.jpg', compress_type=zipfile.ZIP_DEFLATED)
         jungle_zip.close()
         return result
 
